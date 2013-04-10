190b024dc077e03db87e56be983c4e1c7c89468d
Marcos Daniel Petry <marcospetry@gmail.com>
2013-04-10 17:21:50 -0300

    discovering allowed methods

--- apimanagerclient/service.py
+++ apimanagerclient/service.py
@@ -4,13 +4,36 @@ import json
 import requests
 
 
+class Resource(object):
+
+    def __init__(self, name, service_url):
+        self.url = "{0}/{1}".format(service_url, name)
+        self.methods = self.get_allowed_methods()
+
+    def _get_schema(self):
+        schema_url = '{0}/{1}'.format(self.url, 'schema')
+        response = requests.get(url=schema_url, params={'format': 'json'})
+        resource_dict = json.loads(response.content)
+        return resource_dict
+
+    def get_allowed_methods(self):
+        schema = self._get_schema()
+        return schema['allowed_list_http_methods']
+
+
 class Service(object):
     def __init__(self, url, version):
-        self.api_url = "http://{0}/{1}".format(url,version)
+        self.url = url
         self.version = version
-        self.resources = []
+        schema_url = "{0}/{1}".format(url, version)
+        self.resources = self._get_service_resources(schema_url)
+        self._create_resources_attributes()
 
-    def get_resources(self):
-        response = requests.get(url=self.api_url, params={'format':'json'})
+    def _get_service_resources(self, schema_url):
+        response = requests.get(url=schema_url, params={'format': 'json'})
         resource_dict = json.loads(response.content)
-        return resource_dict.keys()
+        return resource_dict
+
+    def _create_resources_attributes(self):
+        for key, value in self.resources.items():
+            self.__setattr__(key, Resource(name=key, service_url=self.url))
--- apimanagerclient/tests.py
+++ apimanagerclient/tests.py
@@ -5,9 +5,10 @@ import json
 import unittest
 from mock import patch, MagicMock
 from apimanagerclient import Service
+from apimanagerclient.service import Resource
 
 
-class SchemaMock(MagicMock):
+class ServiceSchemaMock(MagicMock):
     content = json.dumps({
         "extensions": {
             "list_endpoint": "/v1/extensions/",
@@ -20,30 +21,64 @@ class SchemaMock(MagicMock):
     })
 
 
+
 class ServiceTestCase(unittest.TestCase):
 
 
     def setUp(self):
         super(ServiceTestCase, self).setUp()
         self.request_mock = patch('requests.get').start()
-        self.request_mock.return_value = SchemaMock()
+        self.request_mock.return_value = ServiceSchemaMock()
 
-    def test_should_format_api_url(self):
-        my_service = Service('my-api.com', 'v1')
-        self.assertEqual(my_service.api_url, 'http://my-api.com/v1')
+        patch('apimanagerclient.service.Resource._get_schema').start()
+
+
+        self.my_service = Service('http://my-api.com', 'v1')
+
+    def test_should_have_url(self):
+        self.assertEqual(self.my_service.url, 'http://my-api.com')
+
+    def test_should_have_version(self):
+        self.assertEqual(self.my_service.version, 'v1')
+
 
     def test_should_call_the_get_method(self):
-        my_service = Service('my-api.com', 'v1')
-        my_service.get_resources()
-        self.request_mock.assert_called_once_with(
+        self.request_mock.assert_called_with(
             url='http://my-api.com/v1', params={'format': 'json'}
         )
 
     def test_should_obtain_resources(self):
-        my_service = Service('my-api.com', 'v1')
-        expected_resources = my_service.get_resources()
+        self.assertEqual(self.my_service.resources.keys(), [u'portals', u'extensions'])
 
-        self.assertEqual(expected_resources, [u'portals', u'extensions'])
+    def test_should_create_resources(self):
+        self.assertIsInstance(self.my_service.extensions, Resource)
+        self.assertIsInstance(self.my_service.portals, Resource)
+
+
+class ResourceSchemaMock(MagicMock):
+    content = json.dumps({
+        'allowed_list_http_methods': [
+            "get"
+        ],
+        }
+    )
+
+
+class ResourceTestCase(unittest.TestCase):
+    def setUp(self):
+        super(ResourceTestCase, self).setUp()
+        self.request_mock = patch('requests.get').start()
+        self.request_mock.return_value = ResourceSchemaMock()
+
+        self.my_resource = Resource(name='foo', service_url='http://my-api.com/v1')
+
+    def test_should_obtain_service_url(self):
+        my_resource = Resource(name='foo', service_url='http://my-api.com/v1')
+        self.assertEqual(my_resource.url, 'http://my-api.com/v1/foo')
+
+    def test_should_store_allowed_methods(self):
+        my_resource = Resource(name='foo', service_url='http://my-api.com/v1')
+        self.assertEqual(my_resource.methods, ['get',])
 
 
 if __name__ == '__main__':
