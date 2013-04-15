9381fe8342b55a71c977a616987cbde08ceb9c36
Marcos Daniel Petry <marcospetry@gmail.com>
2013-04-15 11:46:18 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import re
+from setuptools import setup, find_packages, findall
+
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+    return requirements
+
+
+def not_py(file_path):
+    return not(file_path.endswith('.py') or file_path.endswith('.pyc'))
+
+core_packages = find_packages()
+core_package_data = {}
+for package in core_packages:
+    package_path = package.replace('.', '/')
+    core_package_data[package] = filter(not_py, findall(package_path))
+
+setup(
+    name='jsonchema-client',
+    version='0.1',
+    description='python client to Jsonschema APIs',
+    author='Marcos Daniel Petry',
+    author_email='marcospetry@gmail.com',
+    url='git@github.com:globoi/python-jsonschema.git',
+    classifiers=[
+        'Develop    ment Status :: 3 - Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='jsonschema.tests',
+    packages=core_packages,
+    package_data=core_package_data,
+    include_package_data=True,
+    install_requires=parse_requirements('requirements.txt'),
+    tests_require=parse_requirements('requirements_test.txt'),
+)
