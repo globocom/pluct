2e5d708cbf5a1abc75a17226824cf714afec8f1f
Andrews Medina <andrewsmedina@gmail.com>
2013-04-27 19:43:55 -0300

    implemented schema.required.

--- pluct/schema.py
+++ pluct/schema.py
@@ -1,6 +1,15 @@
+from pluct.request import Request
+
+
 class Schema(object):
-    pass
+    def __init__(self, url, required):
+        self.url = url
+        self.required = required
 
 
-def get(uri):
-    return Schema()
+def get(url, auth=None):
+    data = Request("GET", url, auth).process().json
+    return Schema(
+        url=url,
+        required=data.get("required")
+    )
--- pluct/tests/test_schema.py
+++ pluct/tests/test_schema.py
@@ -1,9 +1,36 @@
 from unittest import TestCase
+from mock import patch, Mock
 
 from pluct import schema
 
 
 class SchemaTestCase(TestCase):
-    def test_get_should_returns_a_schema(self):
-        instance = schema.get(uri="http://myapp.com/schema/myschema")
+    @patch("requests.get")
+    def test_get_should_returns_a_schema(self, get):
+        instance = schema.get(url="http://myapp.com/schema/myschema")
         self.assertIsInstance(instance, schema.Schema)
+
+    @patch("requests.get")
+    def test_schema_required(self, get):
+        data = {
+            "title": "app schema",
+            "properties": {
+                "cname": {"type": "string"},
+                "ip": {"type": "string"},
+                "name": {"type": "string"},
+                "platform": {"type": "string"}
+            },
+            "required": ["platform", "name"],
+        }
+        mock = Mock()
+        mock.json = data
+        get.return_value = mock
+        s = schema.get(url="http://myapp.com/schema/myschema")
+        required = ["platform", "name"]
+        self.assertListEqual(required, s.required)
+
+    @patch("requests.get")
+    def test_schema_url(self, get):
+        url = "http://ble.com/schema"
+        s = schema.get(url=url)
+        self.assertEqual(url, s.url)
