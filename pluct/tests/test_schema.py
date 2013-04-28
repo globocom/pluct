80741ee1ce739e1c522df1dbe9fb0a71ffb0a2ce
Andrews Medina <andrewsmedina@gmail.com>
2013-04-27 22:24:30 -0300

    simplied schema tests.

--- pluct/tests/test_schema.py
+++ pluct/tests/test_schema.py
@@ -5,7 +5,8 @@ from pluct import schema
 
 
 class SchemaTestCase(TestCase):
-    def setUp(self):
+    @patch("requests.get")
+    def setUp(self, get):
         self.data = {
             "title": "app schema",
             "properties": {
@@ -28,54 +29,25 @@ class SchemaTestCase(TestCase):
             ],
             "required": ["platform", "name"],
         }
-
-    @patch("requests.get")
-    def test_get_should_returns_a_schema(self, get):
-        instance = schema.get(url="http://myapp.com/schema/myschema")
-        self.assertIsInstance(instance, schema.Schema)
-
-    @patch("requests.get")
-    def test_schema_required(self, get):
-        mock = Mock()
-        mock.json = self.data
+        mock = Mock(json=self.data)
         get.return_value = mock
-        s = schema.get(url="http://myapp.com/schema/myschema")
-        required = ["platform", "name"]
-        self.assertListEqual(required, s.required)
+        self.url = "http://app.com/myschema"
+        self.result = schema.get(url=self.url)
 
-    @patch("requests.get")
-    def test_schema_title(self, get):
-        mock = Mock()
-        mock.json = self.data
-        get.return_value = mock
-        s = schema.get(url="http://myapp.com/schema/myschema")
-        title = "app schema"
-        self.assertEqual(title, s.title)
+    def test_get_should_returns_a_schema(self):
+        self.assertIsInstance(self.result, schema.Schema)
 
-    @patch("requests.get")
-    def test_schema_properties(self, get):
-        mock = Mock()
-        mock.json = self.data
-        get.return_value = mock
-        s = schema.get(url="http://myapp.com/schema/myschema")
-        properties = {
-            "cname": {"type": "string"},
-            "ip": {"type": "string"},
-            "name": {"type": "string"},
-            "platform": {"type": "string"}
-        }
-        self.assertDictEqual(properties, s.properties)
+    def test_schema_required(self):
+        self.assertListEqual(self.data["required"], self.result.required)
 
-    @patch("requests.get")
-    def test_schema_links(self, get):
-        mock = Mock()
-        mock.json = self.data
-        get.return_value = mock
-        s = schema.get(url="http://myapp.com/schema/myschema")
-        self.assertListEqual(self.data["links"], s.links)
+    def test_schema_title(self):
+        self.assertEqual(self.data["title"], self.result.title)
 
-    @patch("requests.get")
-    def test_schema_url(self, get):
-        url = "http://ble.com/schema"
-        s = schema.get(url=url)
-        self.assertEqual(url, s.url)
+    def test_schema_properties(self):
+        self.assertDictEqual(self.data["properties"], self.result.properties)
+
+    def test_schema_links(self):
+        self.assertListEqual(self.data["links"], self.result.links)
+
+    def test_schema_url(self):
+        self.assertEqual(self.url, self.result.url)
