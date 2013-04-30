35a8d245783d9bc94b19d3ab96f15dce2e8ec98b
Andrews Medina <andrewsmedina@gmail.com>
2013-04-30 11:17:52 -0300

    removed request.get_url.

--- pluct/request.py
+++ pluct/request.py
@@ -1,7 +1,5 @@
 import json
-import re
 import requests
-from uritemplate import expand
 
 
 class Request(object):
@@ -12,30 +10,25 @@ class Request(object):
         self.auth = auth
 
     def _get(self, **kwargs):
-        url = self.get_url(**kwargs)
-        return requests.get(url=url, headers=self.get_headers())
+        return requests.get(url=self.href, headers=self.get_headers())
 
     def _post(self, **kwargs):
         data = kwargs.pop('data')
-        url = self.get_url(**kwargs)
-        return requests.post(url=url, data=json.dumps(data),
+        return requests.post(url=self.href, data=json.dumps(data),
                              headers=self.get_headers())
 
     def _patch(self, **kwargs):
         data = kwargs.pop('data')
-        url = self.get_url(**kwargs)
-        return requests.patch(url=url, data=json.dumps(data),
+        return requests.patch(url=self.href, data=json.dumps(data),
                               headers=self.get_headers())
 
     def _put(self, **kwargs):
         data = kwargs.pop('data')
-        url = self.get_url(**kwargs)
-        return requests.put(url=url, data=json.dumps(data),
+        return requests.put(url=self.href, data=json.dumps(data),
                             headers=self.get_headers())
 
     def _delete(self, **kwargs):
-        url = self.get_url(**kwargs)
-        return requests.delete(url=url, headers=self.get_headers())
+        return requests.delete(url=self.href, headers=self.get_headers())
 
     def get_headers(self):
         header = {
@@ -62,22 +55,3 @@ class Request(object):
             'DELETE': self._delete,
         }
         return request_type_by_method[self.method]
-
-    def get_url(self, **kwargs):
-        right_parameters = re.compile('{([^\}]+)}').findall(self.href)
-        if set(kwargs.keys()).symmetric_difference(right_parameters):
-            wrong_parameters = set(kwargs.keys()).difference(right_parameters)
-            if wrong_parameters:
-                message_wrong_params = "Wrong parameters: \"{0}\".".format(
-                    ", ".join(wrong_parameters))
-            else:
-                message_wrong_params = "You did not set any parameter."
-
-            if right_parameters:
-                message_right_params = "Valid parameters: \"{0}\"".format(
-                    ", ".join(right_parameters))
-            else:
-                message_right_params = "This method takes no parameter."
-            raise TypeError("{0} {1}".format(message_wrong_params,
-                                             message_right_params))
-        return expand(self.href, kwargs)
