676f9507b95e552aa579a2684510192fbc21898c
Guilherme Cirne <gcirne@gmail.com>
2013-07-31 18:22:35 -0300

    Removes commented code.

--- pluct/resource.py
+++ pluct/resource.py
@@ -14,7 +14,6 @@ def add_methods(resource, s, auth=None):
         method = link.get("method", "GET")
         href = link.get("href")
         rel = link.get("rel")
-        # href = expand(href, resource.data)
         method_class = Request(method, href, auth, resource)
         setattr(resource, rel, method_class.process)
 
