c95115e6a584903d3b43a9624144df0a15c68a4b
rodsenra <rodsenra@gmail.com>
2013-11-22 17:51:23 -0200

    Fix regular expression in setup.py to avoid -r puppet/modules/brainiak/files/requirements.txt' in install_requires

--- setup.py
+++ setup.py
@@ -13,8 +13,11 @@ def parse_requirements(file_name):
             requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
         elif re.match(r'\s*-f\s+', line):
             pass
+        elif re.match(r'\s*-r\s+', line):
+            pass
         else:
             requirements.append(line)
+    print requirements
     return requirements
 
 
