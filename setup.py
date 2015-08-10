62c3dcbe260a81c2ad942923cc3690882deeb1f7
Enrico <rico.bl@gmail.com>
2015-08-10 15:17:50 -0300

    Bump version: 1.2.3 → 1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pluct',
-    version='1.2.3',
+    version='1.2.4',
     description='JSON Hyper Schema client',
     long_description=open('README.rst').read(),
     author='Marcos Daniel Petry',
